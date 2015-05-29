71648f464c765ab1fb8bcd7566f29abd3de8082d
Quwert <gwirxe@gmail.com>
2015-05-29 11:46:11 +0200

    um URL eingabe erweitert

--- test/makejunk.sh
+++ test/makejunk.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# if [ -n $1 ] && [ -n $2 ] && [ -n $2 ]; then
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+if [ -z "$1" ]; then
 	echo "ALL options must be specified"
         exit
 fi
 
 if [ $1 == "-h" ]; then
 	echo
-	echo "Usage: makejunk.sh [n] [o] [y/n]"
+	echo "Usage: makejunk.sh [URL] [n] [o] [y/n]"
+	echo "URL: URL to call"
 	echo "n:   Number of calls to wget"
 	echo "o:   output File for speeds [MB/s]"
 	echo "y/n: start gnuplot after wget-loop finishes?"
@@ -17,25 +17,31 @@ if [ $1 == "-h" ]; then
 	exit
 fi
 
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
+        echo "ALL options must be specified"
+        exit
+fi
+
 tempo=tempo.file
+rm $tempo
 echo "Request progress:"
 
-for ((i=0;$i <= $(($1));i++))do
-	wget --no-check-certificate keller-pc -O /dev/null 2>> $tempo
+for ((i=0;$i <= $(($2));i++))do
+	wget --no-check-certificate $1 -O /dev/null 2>> $tempo
 	printf "#"
 done
 
 echo " done"
 
-grep "MB\/s" $tempo | cut -d " " -f 3 | sed -e 's/(//' > $2
+grep "MB\/s" $tempo | cut -d " " -f 3 | sed -e 's/(//' > $3
 
 rm $tempo
 
-if [ $3 == "n"  ]; then
+if [ $4 == "n"  ]; then
 	echo "not starting gnuplot"
 else
-	if [ $3 == "y" ]; then
-		gnuplot -e "set term x11 persist;set grid ;p '$2' w l"
+	if [ $4 == "y" ]; then
+		gnuplot -e "set term x11 persist;set grid ;p '$3' w l"
 	else
 		echo "Newton you have wrong, light is wave!"
 	fi
