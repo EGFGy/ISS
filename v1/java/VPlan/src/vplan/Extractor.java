99f7e49c2d9d0f6928086340c305722556421ea0
Ruben <switchhax@users.noreply.github.com>
2015-11-23 16:08:26 +0100

    Update Extractor.java

--- v1/java/VPlan/src/vplan/Extractor.java
+++ v1/java/VPlan/src/vplan/Extractor.java
@@ -12,8 +12,8 @@ public class Extractor {
     private static final ArrayList<String> lines = new ArrayList<>();
 
     public Extractor(String str) {
-        Extractor.convStr = str;        
-        extractTitel(convStr);        
+        Extractor.convStr = str;
+        extractTitel(convStr);
         convStr = removeWeirdness(convStr, "ä");
         extractStand(convStr);
         getMessages(convStr);
@@ -39,25 +39,25 @@ public class Extractor {
         String statt = "";
         String lehrer = "";
         String sonstiges = "";
-        for (int z = 0; z < lines.size() / 10; z++) {
-            int i = 10 * z;
+        for (int z = 0; z < lines.size() / 9; z++) {
+            int i = 9 * z;
             String zeile = lines.get(i);
             zeile = lines.get(i + 1);
-            klasse = getPartOfString(zeile, "Vertretungen\">", "</td>");
+            klasse = getPartOfString(zeile, "Vertretungen\">", "<");
             zeile = lines.get(i + 2);
-            std = getPartOfString(zeile, "Vertretungen\">", "</td>");
+            std = getPartOfString(zeile, "Vertretungen\">", "<");
             zeile = lines.get(i + 3);
-            vertretung = getPartOfString(zeile, "Vertretungen\">", "</td>");
+            vertretung = getPartOfString(zeile, "Vertretungen\">", "<");
             zeile = lines.get(i + 4);
-            fach = getPartOfString(zeile, "Vertretungen\">", "</td>");
+            fach = getPartOfString(zeile, "Vertretungen\">", "<");
             zeile = lines.get(i + 5);
-            raum = getPartOfString(zeile, "Vertretungen\">", "</td>");
+            raum = getPartOfString(zeile, "Vertretungen\">", "<");
             zeile = lines.get(i + 6);
-            statt = getPartOfString(zeile, "Vertretungen\">", "</td>");
+            statt = getPartOfString(zeile, "Vertretungen\">", "<");
             zeile = lines.get(i + 7);
-            lehrer = getPartOfString(zeile, "Vertretungen\">", "</td>");
+            lehrer = getPartOfString(zeile, "Vertretungen\">", "<");
             zeile = lines.get(i + 8);
-            sonstiges = getPartOfString(zeile, "Vertretungen\">", "</td>");
+            sonstiges = getPartOfString(zeile, "Vertretungen\">", "<");
             Element item = new Element(klasse, std, vertretung, fach, raum, statt, lehrer, sonstiges);
             elements.add(item);
         }
@@ -68,6 +68,7 @@ public class Extractor {
 
     private void extractStand(String str) {
         stand = getPartOfString(str, "Stand:", "<");
+        System.out.println(stand);
     }
 
     private void getMessages(String str) {
@@ -79,13 +80,14 @@ public class Extractor {
 
     private String delBreak(String s) {
         s = s.replaceAll("<BR /> <BR /> ", "\n");
+        s = s.replaceAll("<BR />", "\n");
         return s;
     }
-    
-    private void extractTitel(String str) {    
-        tag = getPartOfString(str, "<Title>", "<");
+
+    private void extractTitel(String str) {
+        tag = getPartOfString(str, "<Title>", "</Title");
         tag = removeWeirdness(tag, "ü");
-        String s = "Ehrenbürg-Gymnasium Forchheim Vertretungsplan für ";
+        String s = "Gymnasium Ehrenbürg Forchheim Vertretungsplan für";
         int len = s.length();
         if (tag.startsWith(s)) {
             tag = tag.substring(len);
@@ -94,7 +96,7 @@ public class Extractor {
         }
     }
 
-    private static String getPartOfString(String s, String s1, String s2) {
+    public static String getPartOfString(String s, String s1, String s2) {
         String erg = "";
         int m = s.indexOf(s1);
         if (m > -1) {
@@ -111,17 +113,25 @@ public class Extractor {
         s = s.replaceAll("�", weird);
         return s;
     }
-    
-    public String convertElementsToString(){
+
+    public String convertElementsToString() {
         String work = "";
-        work = "Tag:," + tag.replace(",", "") + "\n";
-        work = work + "Stand:," + stand + "\n\n";
-        work = work + "Messages:,\n" + messages.replace("ä", "�") + "\n\n";
+//        work = "Tag:," + tag.replace(",", "") + "\n";
+//        work = work + "Stand:," + stand + "\n\n";
+//        work = work + "Messages:,\n" + messages.replace("ä", "�") + "\n\n";
         work = work + "klasse,std,vertretung,fach,raum,statt,lehrer,sonstiges" + "\n";
-        for(Element e: elements){
+        for (Element e : elements) {
             work += e.convertToCSV();
             work += "\n";
         }
+        return work;
+    }
+    
+    public String getData(){
+        String work = "";
+        work = "Tag: " + tag.replace(",", "") + "\n";
+        work = work + "Stand: " + stand + "\n\n";
+        work = work + "Nachrichten:\n" + messages.replace("ä", "") + "\n\n";
         return work;
     }
 
@@ -160,8 +170,6 @@ public class Extractor {
     public static void setConvStr(String convStr) {
         Extractor.convStr = convStr;
     }
-    
-    
 
     private class Element {
 
@@ -205,15 +213,15 @@ public class Extractor {
         public String getSonstiges() {
             return sonstiges;
         }
-        
-        public String removeSpace(String s){
-            if(s.equals("")){
+
+        public String removeSpace(String s) {
+            if (s.equals("")) {
                 return "null";
-            }else{
+            } else {
                 return s;
             }
         }
-        
+
         public Element(String klasse, String std, String vertretung, String fach, String raum, String statt, String lehrer, String sonstiges) {
             this.klasse = klasse;
             this.std = std;
@@ -224,12 +232,13 @@ public class Extractor {
             this.lehrer = lehrer;
             this.sonstiges = sonstiges;
         }
+
         @Override
         public String toString() {
             return "Element{" + "klasse=" + klasse + ", std=" + std + ", vertretung=" + vertretung + ", fach=" + fach + ", raum=" + raum + ", statt=" + statt + ", lehrer=" + lehrer + ", sonstiges=" + sonstiges + '}';
         }
-        
-        public String convertToCSV(){
+
+        public String convertToCSV() {
             klasse = removeSpace(klasse);
             std = removeSpace(std);
             vertretung = removeSpace(vertretung);
@@ -238,7 +247,7 @@ public class Extractor {
             statt = removeSpace(statt);
             lehrer = removeSpace(lehrer);
             sonstiges = removeSpace(sonstiges);
-            return klasse + "," + std + "," + vertretung + "," + fach.replace("ä","Ü") + "," + raum + "," + statt + "," + lehrer + "," + sonstiges;
+            return klasse + "," + std + "," + vertretung + "," + fach.replace("ä", "Ü") + "," + raum + "," + statt + "," + lehrer + "," + sonstiges;
         }
     }
 }
