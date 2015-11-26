fc4e183fe83191815ed555ed44bea71de756ff99
Quwert <gwirxe@gmail.com>
2015-11-26 22:58:18 +0100

    Kommentare

--- v3/C/backend/all_messages.c
+++ v3/C/backend/all_messages.c
@@ -43,9 +43,10 @@ int main(int argc, char ** argv){
 
 
     if(verify_sid(&check_person)){
+		/** Restliche Informationen über die aktuelle Person in Erfahrung bringen */
 		get_person_by_sid(&check_person);
-		httpCacheControl("no-store, no-cache, must-revalidate, max-age=0");
-		httpHeader(HTML);
+
+
 
 		char * s_offest=NULL;
 		if(extract_QUERY_data(&datCGI, "offset", &s_offest) != -1){
@@ -61,11 +62,16 @@ int main(int argc, char ** argv){
 			no_older=true;
 		}
 
+		/** HTTP + HTML ab hier */
+		httpCacheControl("no-store, no-cache, must-revalidate, max-age=0");
+		httpHeader(HTML);
+
 		print_html_pure_head_menu("Anzeige von allgemeinen Meldungen", "Allgemeines", MAIN);
 		puts("<a id='scroll-top' href='#'></a>"); //"Nach-oben"-Knopf // http://icons8.com/ https://icons8.com/license/
 		//Nachrichten ab hier
 		puts("<div class='content'>");
 
+		/** Wenn ein Lehrer die Seite aufruft, das Eingabefeld anzeigen*/
         if(check_person.isTeacher)puts("<div id='message-form'><form style='border-radius: 1em; padding: 1em;' action='/cgi-bin/post_message.cgi' method='POST' enctype='application/x-www-form-urlencoded'>\n\
 			  <label style='font-weight: bold;' for='ti'>Titel</label><input style='display: block;' name='titel' id='ti' type='text' onchange='main.countLettersInThis(this); main.validateAllInput();' onkeyup='main.countLettersInThis(this); main.validateAllInput();'>\n\
 			  <label style='font-weight: bold;' for='tex'>Text</label><textarea style='display: block;' name='meldung' id='tex' onchange='main.countLettersInThis(this); main.validateAllInput();' onkeyup='main.countLettersInThis(this); main.validateAllInput();'></textarea>\n\
@@ -74,6 +80,8 @@ int main(int argc, char ** argv){
 			");
 
 		printf("<span>Seite %d</span>\n", no_older ? offset : offset+1);
+
+		/** Die einzelnen Meldungen der Reihe nach auflisten*/
 		for(int i=0; i<number; i++){
 			person pers;
 			init_person(&pers);
@@ -82,6 +90,8 @@ int main(int argc, char ** argv){
 
 			puts("<div class='messageBox'>");
 			printf("	<h2 class='content-subhead'>%s</h2>\n	<p>%s</p>\n", (all_messages+i)->title, (all_messages+i)->message);
+
+			/** Ersteller und Uhrzeit zu der die Meldung verfasst wurde anzeigen*/
 			if(get_person_by_id(&pers)){
 				printf("	<h3 style='font-size: 12px; font-style: italic;' class='message-info'>Um %s von %s %s erstellt</h3>", (all_messages+i)->s_created, pers.first_name, pers.name );
 			}else{
@@ -102,6 +112,8 @@ int main(int argc, char ** argv){
 		puts("</div>"); //zu Content
 		puts("<div style='text-align: center;'>");
 
+
+		/** Navigationstasten */
 		if(offset>0){
 			printf("<a class='pure-menu-link' style='display: inline; color: black;' href='/cgi-bin/all_messages.cgi?offset=%d'>&#x2770; Neuere</a>", offset-1);
 			puts("<a class='pure-menu-link' style='display: inline; color: black;' href='/cgi-bin/all_messages.cgi'>Neueste &#x21ef;</a>");
